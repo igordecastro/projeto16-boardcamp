976755c052c635f4a923719160200ea5afd980e6
Igor de Castro <igordecastro07@gmail.com>
2022-12-11 19:23:50 -0300

    feat: update customer

--- src/controllers/customers.controller.js
+++ src/controllers/customers.controller.js
@@ -1,39 +1,39 @@
 import connection from "../server.js";
 
 export async function findCustomers(req, res) {
-    const { cpf } = req.query;
-    
-    try {
-      if (cpf) {
-        const filteredCustomers = await connection.query(
-          `SELECT * FROM customers WHERE cpf Ilike '${cpf}%';`
-        );
-        return res.send(filteredCustomers.rows);
-      }
-  
-      const customers = await connection.query(
-        'SELECT * FROM customers;'
+  const { cpf } = req.query;
+
+  try {
+    if (cpf) {
+      const filteredCustomers = await connection.query(
+        `SELECT * FROM customers WHERE cpf Ilike '${cpf}%';`
       );
-      res.send(customers.rows);
-    } catch (err) {
-      res.status(500).send(err.message);
+      return res.send(filteredCustomers.rows);
     }
+
+    const customers = await connection.query("SELECT * FROM customers;");
+    res.send(customers.rows);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 }
 
 export async function findCustomerById(req, res) {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    try{
-        const customer = await connection.query("SELECT * FROM customers WHERE id=$1", [id]);
+  try {
+    const customer = await connection.query(
+      "SELECT * FROM customers WHERE id=$1",
+      [id]
+    );
 
-        if(customer.rows.length === 0) {
-            return res.sendStatus(404);
-        }
-        res.send(customer.rows);
-
-    } catch(err) {
-        res.status(500).send(err.message)
+    if (customer.rows.length === 0) {
+      return res.sendStatus(404);
     }
+    res.send(customer.rows);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 }
 
 export async function createCustomer(req, res) {
@@ -45,6 +45,21 @@ export async function createCustomer(req, res) {
       [name, phone, cpf, birthday]
     );
     res.sendStatus(201);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+}
+
+export async function updateCustomer(req, res) {
+  const { name, phone, cpf, birthday } = res.locals.customer;
+  const { id } = req.params;
+
+  try {
+    await connection.query(
+      "UPDATE customers SET name=$1, phone=$2, cpf=$3, birthday=$4 WHERE id=$5;",
+      [name, phone, cpf, birthday, id]
+    );
+    res.sendStatus(200);
   } catch (err) {
     res.status(500).send(err.message);
   }
--- src/routes/customers.routes.js
+++ src/routes/customers.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 
-import { findCustomers, createCustomer, findCustomerById } from '../controllers/customers.controller.js'
+import { findCustomers, createCustomer, updateCustomer, findCustomerById } from '../controllers/customers.controller.js'
 import validateCustomer from "../models/validateCustomer.middleware.js";
 
 const router = Router();
 
 router.get("/customers", findCustomers);
 router.get("/customers/:id", findCustomerById);
+router.put("/customers/:id", validateCustomer ,updateCustomer);
 router.post("/customers", validateCustomer ,createCustomer);
 
 export default router;
